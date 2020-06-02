a388689837edf2f2f170529698ff5007abc6241e
Aaron-LHR <lianhaoran999@gmail.com>
2020-06-02 16:38:09 +0800

    练习

--- src/Client/client1.java
+++ src/Client/client1.java
@@ -1,6 +1,6 @@
 package Client;
 
-public class client {
+public class client1 {
     public static void main(String[] args) {
         new Thread(new SendThread(6666, "localhost", 9999)).start();
         new Thread(new ReceiveThread(7777)).start();
--- src/Client/client2.java
+++ src/Client/client2.java
@@ -1,6 +1,6 @@
 package Client;
 
-public class receiver {
+public class client2 {
     public static void main(String[] args) {
         new Thread(new SendThread(8888, "localhost", 7777)).start();
         new Thread(new ReceiveThread(9999)).start();
