95e3b6c2c5d9b1c3eed546de49be5f4f5bf0e8c9
XiXi1103 <1239174672@qq.com>
2020-06-02 22:04:12 +0800

    实现注册

---
+++ src/Server/Register.java
@@ -0,0 +1,31 @@
+package Server;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Register extends Thread{
+    public String username;
+    private String passwd;
+    private Socket socket;
+    public Register(String username,String passwd,Socket socket) {
+        this.username=username;
+        this.passwd=passwd;
+        this.socket=socket;
+    }
+    @Override
+    public void run() {
+        try {
+            DataOutputStream out=new DataOutputStream(socket.getOutputStream());
+            DataInputStream in=new DataInputStream(socket.getInputStream());
+            FileWriter writer = new FileWriter(new File("Account"), true);
+            if (Server.account.get(username)!=null){
+                out.writeUTF("0");
+            }
+            writer.write("\n"+username+" "+passwd);
+            writer.close();
+            out.writeUTF("1");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
