a89da6b9a507b1c3070259e33f7b6037e8dd0568
XiXi1103 <1239174672@qq.com>
2020-06-11 20:47:12 +0800

    修复bug

--- src/main/java/src/Server/bla.java
+++ src/main/java/src/Server/bla.java
@@ -2,6 +2,7 @@ package src.Server;
 
 import java.io.*;
 import java.net.InetAddress;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +10,14 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class bla {
     public static void main(String[] args) {
-        
+        List<String>list=new ArrayList<>();
+        list.add("131");
+        list.add("222");
+        list.add("333");
+        list.add(0,list.get(0).replace("1",""));
+        list.remove(1);
+        for (String s:list){
+            System.out.println(s);
+        }
     }
 }
